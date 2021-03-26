f339a08336be1069f79f63c9e02e29aa5140d758
JorgeJoseAbad <jorgejoseabad@gmail.com>
2021-03-26 09:26:05 +0100

    First steep complete, fetching data from API, json.

--- code/routes/index.js
+++ code/routes/index.js
@@ -3,22 +3,68 @@ var router = express.Router();
 const fetch = require("node-fetch");
 const fileType = require('file-type');
 
+const mainUrl = "https://my-json-server.typicode.com/jorgejoseabad/fake-apis";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 //primera ruta a api
-router.get('/myPost',function (req,res,next) {
-  console.log("estoy en fetch my post");
+router.get('/posts',function (req,res,next) {
+  console.log("estoy en fetch posts");
   //ok si está funcionando
-  fetch('https://my-json-server.typicode.com/jorgejoseabad/fake-apis/posts')
+  fetch(`${mainUrl}` +'/posts')
   .then(res => res.json())
   .then(json => {
     console.log(json);
     res.send(json);
   })
   .catch(err=>console.log("Recojo error",err))
+})
+
+router.get('/comments',function (req,res,next) {
+   console.log("estoy en fetch comments ");
+   fetch(`${mainUrl}` +'/comments')
+   .then(res => res.json())
+   .then(json => {
+     console.log(json);
+     res.send(json)
+   })
+   .catch(err => console.log("Recojo error",err))
+})
+
+router.get('/products',function (req,res,next) {
+   console.log("estoy en fetch comments ");
+   fetch(`${mainUrl}` +'/products')
+   .then(res => res.json())
+   .then(json => {
+     console.log(json);
+     res.send(json)
+   })
+   .catch(err => console.log("Recojo error",err))
+})
+
+router.get('/profile',function (req,res,next) {
+   console.log("estoy en fetch comments ");
+   fetch(`${mainUrl}` +'/profile')
+   .then(res => res.json())
+   .then(json => {
+     console.log(json);
+     res.send(json)
+   })
+   .catch(err => console.log("Recojo error",err))
+})
+
+router.get('/db',function (req,res,next) {
+   console.log("estoy en fetch comments ");
+   fetch(`${mainUrl}` +'/db')
+   .then(res => res.json())
+   .then(json => {
+     console.log(json);
+     res.send(json)
+   })
+   .catch(err => console.log("Recojo error",err))
 })
 
 module.exports = router;
